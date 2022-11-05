cd037ad6d9a03a151537541bca7cddd9438169df
L-dongkyung <ckldk91m@naver.com>
2022-11-05 20:18:53 +0900

    경로 파라미터 정의 및 Enum 클래스 사용 경로 파라미터 정의 방법. Enum 클래스를 이용해서 값을 열거형으로 정의. 경로에 파일에 대한 경로를 받을 경우 처리 방법.

--- README.md
+++ README.md
@@ -1,5 +1,30 @@
-### fastapi-tutorial
+# fastapi-tutorial
 
 ## fastapi install
+```bash
+$ pip install "fastapi[all]"
+```
+## path parameters
+app 데코레이터의 경로에 parameter를 추가할 수 있습니다.  
+url의 query string으로 전달 받은 str을 타입 선언을 할 경우 fastapi가 파싱합니다.  
+타입 파싱에 실패할 경우 오류를 발생합니다.  
+path의 parameter와 함수의 parameter명을 일치시켜야합니다.  
+경로를 정의 할때에는 순서를 유의해야합니다.  
+## Enum class
+Enum 클래스를 사용하여 사전에 선택 값을 정의할 수 있습니다.
+```python
+from enum import Enum
 
-> $ pip install "fastapi[all]"
+class ClassName(Enum):
+    ...
+```
+함수의 parameter 타입 어노테이션을 정의하여 경로 매개변수를 만듭니다.
+## file path parameters
+경로 파라미터에 `:path`를 추가하여 경로임을 선언합니다.  
+이 경로 파라미터는 `/`로 시작하는 str이어야합니다.  
+url의 경우 `server.com/files//home/dir/file.ext`형식으로 입력합니다.
+```python
+@app.get("/files/{file_path:path")
+async def read_file(file_path: str):
+    ...
+```
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from enum import Enum
+
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -5,3 +7,66 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+# 경로의 쿼리스트링 변수와 함수의 파라미터 이름이 같아야합니다. 다르면 에러 발생.
+@app.get("/items/{item_id}")
+async  def read_item(item_id):
+    """
+    query string으로 인수를 받아서 처리 \n
+    :url: server.com/items/item_id
+    :param item_id:
+    :return:
+    """
+    return {"item_id": item_id}
+
+# 경로에 중접이 될 경우 위에서 부터 인식합니다.
+# 아래의 두 경로 위치가 바뀔 경우 users/me 경로는 실행 할 수 없습니다.
+@app.get("/users/me")
+async def read_user_me():
+    return {"user_id": "the current user"}
+
+
+@app.get("/users/{user_id}")
+async def read_user(user_id: int):
+    return {"user_id": user_id}
+
+
+# 이 함수는 호출 할 수 없습니다. 위의 read_user에서 모두 실행합니다.
+# 그러므로 경로를 정의 할때에 주의 해야합니다.
+@app.get("/users/{user_name}")
+async def read_user_name(user_name: str):
+    return {"user_name": user_name}
+
+
+class ModelName(str, Enum):
+    """
+    str을 상속하는 이유는 쿼리스트링의 인수가 str형이라는 것을 알려줘 렌더링 할 수 있게 해줍니다.
+    Enum을 통해서 swagger에서 파라미터 설정을 선택할 수 있습니다.
+    """
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
+
+# 파일 path 매개변수 사용
+@app.get("/files/{file_path:path}")
+async def read_file(file_path: str):
+    """
+    /로 시작하는 파일의 경로를 받을 수 있습니다.
+    url에는 이중 슬래시 // 가 입력됩니다.
+    :param file_path: /로 시작하는 경로.
+    :return:
+    """
+    return {"file_path": file_path}
+
